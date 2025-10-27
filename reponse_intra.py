7d1fd64716d61c87178cfb86ae09be760a598cd0
6250574 <6250574@etu.cegepsth.qc.ca>
2025-10-27 12:16:18 -0400

    intra 1

--- reponse_intra.py
+++ reponse_intra.py
@@ -1,3 +1,55 @@
 ##########################################
 # Nom, Prénom, <Numéro de DA>
 ##########################################
+## Beaulieu, Naomy, <6250574>
+
+import random
+#généré seed pour afficher 10 résultat
+random.seed(10)
+
+
+def reponse(random):
+    pass
+
+
+temperature=reponse(random)
+#généré random pour dans l'intervalle 20 à 35 avec un décimal. le 2 représente 1 décimal.
+print(random.uniform(20, 35, '2'))
+temperature=float(input("jour n"))
+#numéroté les jour de 1 à 10.
+n=(1, 10)
+if temperature <24:
+    print (" trop froid")
+elif temperature >30:
+    print ("trop chaud")
+else:
+    print ("ok")
+    #and est pour afficher fin lorsque toute les réponse ci-haut seront afficher.
+    and:
+    "afficher_fin"
+#pas de réponse en cause de l'erreur a`la ligne 17
+
+
+
+
+# numéro 2
+restart()
+#restart() pour recomencer un numéro
+def question_2():
+import random
+print(random.uniform(10,30))
+#choisie la réponse 26, car aucune réponse du a`l'erreur ligne 28
+(26*(3.1416/1.5))*10
+import matplotlib.pyplot as plt
+plt.figure(figsize=(2,10))
+plt.plot([0,10],[26,545], "*b")
+#ligne de tirets rouge à 500, car aucune donnée à 50000.
+plt.plot([0,10],[500], "r--")
+plt.grid()
+plt.show()
+
+
+
+
+
+
